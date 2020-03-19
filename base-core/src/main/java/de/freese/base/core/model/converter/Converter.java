218bae4cfdd6d1acd446a8cc4318d86e6b1d9762
tfreese <commercial@freese-home.de>
2020-03-19 18:11:58 +0100

    ObjectCoverter

---
+++ base-core/src/main/java/de/freese/base/core/model/converter/Converter.java
@@ -0,0 +1,77 @@
+/**
+ * Created: 19.03.2020
+ */
+
+package de.freese.base.core.model.converter;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * @author Thomas Freese
+ * @param <SOURCE> Typ der Quelle
+ * @param <TARGET> Typ des Ziels
+ */
+public class Converter<SOURCE, TARGET>
+{
+    /**
+    *
+    */
+    private final Function<SOURCE, TARGET> fromSource;
+
+    /**
+    *
+    */
+    private final Function<TARGET, SOURCE> fromTarget;
+
+    /**
+     * @param fromSource {@link Function}
+     * @param fromTarget {@link Function}
+     */
+    public Converter(final Function<SOURCE, TARGET> fromSource, final Function<TARGET, SOURCE> fromTarget)
+    {
+        super();
+
+        this.fromSource = Objects.requireNonNull(fromSource, "fromSource required");
+        this.fromTarget = Objects.requireNonNull(fromTarget, "fromTarget required");
+    }
+
+    /**
+     * @param source Object
+     * @return Object
+     */
+    public final TARGET convertFromSource(final SOURCE source)
+    {
+        return this.fromSource.apply(source);
+    }
+
+    /**
+     * @param target Object
+     * @return {@link List}
+     */
+    public final List<SOURCE> convertFromTarget(final Collection<TARGET> target)
+    {
+        return target.stream().map(this::convertFromTarget).collect(Collectors.toList());
+    }
+
+    /**
+     * @param target Object
+     * @return Object
+     */
+    public final SOURCE convertFromTarget(final TARGET target)
+    {
+        return this.fromTarget.apply(target);
+    }
+
+    /**
+     * @param source Object
+     * @return {@link List}
+     */
+    public final List<TARGET> createFromSource(final Collection<SOURCE> source)
+    {
+        return source.stream().map(this::convertFromSource).collect(Collectors.toList());
+    }
+}
---
+++ base-core/src/main/java/de/freese/base/core/model/converter/DateToLongConverter.java
@@ -0,0 +1,39 @@
+/**
+ * Created: 19.03.2020
+ */
+
+package de.freese.base.core.model.converter;
+
+import java.util.Date;
+
+/**
+ * @author Thomas Freese
+ */
+public class DateToLongConverter extends Converter<Date, Long>
+{
+    /**
+     * @param time long
+     * @return {@link Date}
+     */
+    private static Date convertToSource(final long time)
+    {
+        return new Date(time);
+    }
+
+    /**
+     * @param date {@link Date}
+     * @return long
+     */
+    private static long convertToTarget(final Date date)
+    {
+        return date.getTime();
+    }
+
+    /**
+     * Erstellt ein neues {@link DateToLongConverter} Object.
+     */
+    public DateToLongConverter()
+    {
+        super(DateToLongConverter::convertToTarget, DateToLongConverter::convertToSource);
+    }
+}
