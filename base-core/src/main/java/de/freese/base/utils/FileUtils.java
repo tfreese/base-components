d8c6e5bc639ae470ecc3381063aa51b7985599ac
tfreese <commercial@freese-home.de>
2020-04-14 12:30:50 +0200

    utils

---
+++ base-core/src/main/java/de/freese/base/utils/FileUtils.java
@@ -0,0 +1,187 @@
+/**
+ * Created: 14.04.2020
+ */
+
+package de.freese.base.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+/**
+ * @author Thomas Freese
+ */
+public final class FileUtils
+{
+    /**
+     * Kopiert die bestehende Datei nach *.last.
+     *
+     * @param path {@link Path}
+     * @throws IOException Falls was schief geht.
+     */
+    public static void copy(final Path path) throws IOException
+    {
+        if (path == null)
+        {
+            throw new NullPointerException("path required");
+        }
+
+        Path parent = path.getParent();
+        String fileName = path.getFileName().toString();
+        Path last = parent.resolve(fileName + ".last");
+
+        if (!Files.exists(parent))
+        {
+            Files.createDirectories(parent);
+        }
+
+        if (Files.exists(last))
+        {
+            Files.delete(last);
+        }
+
+        Files.copy(path, last); // StandardCopyOption
+    }
+
+    /**
+     * Löscht das Verzeichnis rekursiv inklusive Dateien und Unterverzeichnisse.
+     *
+     * @param file {@link File}
+     * @throws IOException Falls was schief geht.
+     */
+    public static void deleteDirectoryRecursive(final File file) throws IOException
+    {
+        if (file == null)
+        {
+            throw new NullPointerException("file required");
+        }
+
+        deleteDirectoryRecursive(file.toPath());
+    }
+
+    /**
+     * Löscht das Verzeichnis rekursiv inklusive Dateien und Unterverzeichnisse.
+     *
+     * @param path {@link Path}
+     * @throws IOException Falls was schief geht.
+     */
+    public static void deleteDirectoryRecursive(final Path path) throws IOException
+    {
+        if (path == null)
+        {
+            throw new NullPointerException("path required");
+        }
+
+        if (!Files.exists(path))
+        {
+            return;
+        }
+
+        if (!Files.isDirectory(path))
+        {
+            throw new IllegalArgumentException("path is not a dirctory: " + path);
+        }
+
+        Files.walkFileTree(path, new SimpleFileVisitor<Path>()
+        {
+            /**
+             * @see java.nio.file.SimpleFileVisitor#postVisitDirectory(java.lang.Object, java.io.IOException)
+             */
+            @Override
+            public FileVisitResult postVisitDirectory(final Path dir, final IOException exc) throws IOException
+            {
+                Files.delete(dir);
+
+                return FileVisitResult.CONTINUE;
+            }
+
+            /**
+             * @see java.nio.file.SimpleFileVisitor#visitFile(java.lang.Object, java.nio.file.attribute.BasicFileAttributes)
+             */
+            @Override
+            public FileVisitResult visitFile(final Path file, final BasicFileAttributes attrs) throws IOException
+            {
+                Files.delete(file);
+
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    /**
+     * Benennt die bestehende Datei in *.last um.
+     *
+     * @param path {@link Path}
+     * @throws IOException Falls was schief geht.
+     */
+    public static void rename(final Path path) throws IOException
+    {
+        if (path == null)
+        {
+            throw new NullPointerException("path required");
+        }
+
+        Path parent = path.getParent();
+        String fileName = path.getFileName().toString();
+        Path last = parent.resolve(fileName + ".last");
+
+        if (!Files.exists(parent))
+        {
+            Files.createDirectories(parent);
+        }
+
+        if (Files.exists(last))
+        {
+            Files.delete(last);
+        }
+
+        Files.move(path, last); // StandardCopyOption
+    }
+
+    /**
+     * Liefert einen gültigen Dateinamen ohne mehrfache Spaces.<br>
+     * Sonderzeichen werden ersetzt:<br>
+     * <ul>
+     * <li>" -> '
+     * <li>/ -> _
+     * <li>, -> _
+     * <li>* -> _
+     * <li>' ' -> _
+     * </ul>
+     *
+     * @param fileName String
+     * @return String
+     */
+    public static String rewriteFileName(final String fileName)
+    {
+        if ((fileName == null) || (fileName.trim().length() == 0))
+        {
+            return fileName;
+        }
+
+        String name = StringUtils.normalizeSpace(fileName);
+        name = name.replace('"', '\'');
+        name = name.replace('/', '_');
+        name = name.replace(',', '_');
+        name = name.replace('*', '_');
+        name = name.replace(' ', '_');
+
+        // Mehrere Sonderzeichen waren hintereinander.
+        name = name.replace("__", "_");
+        name = name.replace("__", "_");
+
+        return name;
+    }
+
+    /**
+     * Erstellt ein neues {@link FileUtils} Object.
+     */
+    private FileUtils()
+    {
+        super();
+    }
+}
---
+++ base-core/src/test/java/de/freese/base/utils/TestUtils.java
@@ -0,0 +1,47 @@
+/**
+ * Created: 14.04.2020
+ */
+
+package de.freese.base.utils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Thomas Freese
+ */
+public class TestUtils
+{
+    /**
+     *
+     */
+    @Test
+    public void testStringUtilsNormalizeSpace()
+    {
+        String text = "  ab c  d    efg ";
+        String result = StringUtils.normalizeSpace(text);
+        assertEquals("ab c d efg", result);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(' ');
+        sb.append(' ');
+        sb.append('a');
+        sb.append(StringUtils.ASCII_NON_BREAKING_SPACE);
+        sb.append('b');
+        sb.append(' ');
+        sb.append('c');
+        sb.append(' ');
+        sb.append(' ');
+        sb.append('d');
+        sb.append(' ');
+        sb.append(' ');
+        sb.append(' ');
+        sb.append('e');
+        sb.append('f');
+        sb.append('g');
+        sb.append(' ');
+
+        result = StringUtils.normalizeSpace(sb);
+        assertEquals("a b c d efg", result);
+    }
+}
