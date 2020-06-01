ee27ee0517022921b790ab49e2473088183a8f56
tfreese <commercial@freese-home.de>
2020-06-01 10:31:09 +0200

    network interfaces

--- base-net/src/main/java/de/freese/base/utils/NetUtils.java
+++ base-net/src/main/java/de/freese/base/utils/NetUtils.java
@@ -1,9 +1,14 @@
 package de.freese.base.utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Inet4Address;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.UnknownHostException;
 import java.util.Date;
+import java.util.Enumeration;
 import org.apache.commons.net.ntp.NTPUDPClient;
 import org.apache.commons.net.ntp.TimeInfo;
 
@@ -35,6 +40,85 @@ public final class NetUtils
         }
 
         return ipAddrStr;
+    }
+
+    /**
+     * @return String
+     */
+    public static String getHostName()
+    {
+        String hostName = null;
+
+        try
+        {
+            hostName = InetAddress.getLocalHost().getHostName();
+        }
+        catch (Exception ex)
+        {
+            // Bei Betriebssystemen ohne DNS-Konfiguration fuinktioniert InetAddress.getLocalHost nicht !
+        }
+
+        if (hostName == null)
+        {
+            // Cross Platform (Windows, Linux, Unix, Mac)
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("hostname").getInputStream())))
+            {
+                hostName = br.readLine();
+            }
+            catch (Exception ex)
+            {
+                // Ignore
+            }
+        }
+
+        if (hostName == null)
+        {
+            try
+            {
+                // List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+                Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+
+                while (interfaces.hasMoreElements())
+                {
+                    NetworkInterface nic = interfaces.nextElement();
+
+                    // nic.getInterfaceAddresses().forEach(System.out::println);
+
+                    // Stream<InetAddress> addresses = nic.inetAddresses();
+                    Enumeration<InetAddress> addresses = nic.getInetAddresses();
+
+                    while (addresses.hasMoreElements())
+                    {
+                        InetAddress address = addresses.nextElement();
+
+                        if (!address.isLoopbackAddress() && (address instanceof Inet4Address))
+                        {
+                            // IPv4
+                            hostName = address.getHostName();
+                            break;
+                        }
+                        else if (!address.isLoopbackAddress() && !address.isLinkLocalAddress())
+                        {
+                            // IPv6
+                            hostName = address.getHostName();
+                            break;
+                        }
+                        else if (!address.isLoopbackAddress())
+                        {
+                            // IPv6-Link
+                            hostName = address.getHostName();
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Ignore
+            }
+        }
+
+        return hostName;
     }
 
     /**
--- base-net/src/test/java/de/freese/base/net/TestNetUtils.java
+++ base-net/src/test/java/de/freese/base/net/TestNetUtils.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Date;
@@ -99,34 +100,73 @@ public class TestNetUtils
     {
         String hostName = null;
 
-        // hostName = InetAddress.getLocalHost().getHostName();
+        try
+        {
+            hostName = InetAddress.getLocalHost().getHostName();
+            assertNotNull(hostName);
+            System.out.printf("InetAddress.getLocalHost: %s%n", hostName);
+        }
+        catch (Exception ex)
+        {
+            // Bei Betriebssystemen ohne DNS-Konfiguration fuinktioniert InetAddress.getLocalHost nicht !
+            System.out.printf("InetAddress.getLocalHost: %s%n", ex.getMessage());
+        }
 
         // Cross Platform (Windows, Linux, Unix, Mac)
         try (BufferedReader br = new BufferedReader(new InputStreamReader(Runtime.getRuntime().exec("hostname").getInputStream())))
         {
             hostName = br.readLine();
+            assertNotNull(hostName);
+            System.out.printf("CMD 'hostname': %s%n", hostName);
+        }
+        catch (Exception ex)
+        {
+            // Ignore
+            System.out.printf("CMD 'hostname': %s%n", ex.getMessage());
         }
 
-        assertNotNull(hostName);
-
-        // List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
-        Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-
-        while (interfaces.hasMoreElements())
+        try
         {
-            NetworkInterface nic = interfaces.nextElement();
-            Enumeration<InetAddress> addresses = nic.getInetAddresses();
+            // List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
 
-            while (addresses.hasMoreElements())
+            while (interfaces.hasMoreElements())
             {
-                InetAddress address = addresses.nextElement();
+                NetworkInterface nic = interfaces.nextElement();
 
-                if (!address.isLoopbackAddress())
+                // nic.getInterfaceAddresses().forEach(System.out::println);
+
+                // Stream<InetAddress> addresses = nic.inetAddresses();
+                Enumeration<InetAddress> addresses = nic.getInetAddresses();
+
+                while (addresses.hasMoreElements())
                 {
-                    hostName = address.getHostName();
-                    assertNotNull(hostName);
+                    InetAddress address = addresses.nextElement();
+
+                    if (!address.isLoopbackAddress() && (address instanceof Inet4Address))
+                    {
+                        hostName = address.getHostName();
+                        assertNotNull(hostName);
+                        System.out.printf("NetworkInterface IPv4: %s%n", hostName);
+                    }
+                    else if (!address.isLoopbackAddress() && !address.isLinkLocalAddress())
+                    {
+                        hostName = address.getHostName();
+                        assertNotNull(hostName);
+                        System.out.printf("NetworkInterface IPv6: %s%n", hostName);
+                    }
+                    else if (!address.isLoopbackAddress())
+                    {
+                        hostName = address.getHostName();
+                        assertNotNull(hostName);
+                        System.out.printf("NetworkInterface IPv6 Link: %s%n", hostName);
+                    }
                 }
             }
+        }
+        catch (Exception ex)
+        {
+            // Ignore
         }
     }
 
