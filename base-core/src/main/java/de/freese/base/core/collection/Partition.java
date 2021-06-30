9620322e4a2bf73e2e56fcd2b930217a7e1c714a
tfreese <commercial@freese-home.de>
2021-06-30 19:44:47 +0200

    partition list

--- base-core/src/main/java/de/freese/base/core/collection/Partition.java
+++ base-core/src/main/java/de/freese/base/core/collection/Partition.java
@@ -9,7 +9,9 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- * Kopie von org.apache.commons.collections4.ListUtils.Partition.
+ * Kopie von<br>
+ * org.apache.commons.collections4.ListUtils.Partition<br>
+ * io.micrometer.core.instrument.util.AbstractPartition<br>
  *
  * @author Thomas Freese
  * @param <T> Konkreter Typ
@@ -17,34 +19,57 @@ import java.util.Objects;
 public class Partition<T> extends AbstractList<List<T>>
 {
     /**
+     * This rounds up on remainder to avoid orphaning.
      *
+     * @param <T> Type
+     * @param delegate {@link List}
+     * @param partitionSize int
+     * @return int
      */
-    private final List<T> list;
+    private static <T> int partitionCount(final List<T> delegate, final int partitionSize)
+    {
+        int result = delegate.size() / partitionSize;
+
+        return (delegate.size() % partitionSize) == 0 ? result : result + 1;
+
+        // return ((this.delegate.size() + this.partitionSize) - 1) / this.partitionSize;
+    }
 
     /**
      *
      */
-    private final int sizeOfPartition;
+    private final List<T> delegate;
+
+    /**
+     *
+     */
+    private final int partitionCount;
+
+    /**
+     *
+     */
+    private final int partitionSize;
 
     /**
      * Erstellt ein neues {@link Partition} Object.
      *
-     * @param list {@link List}
-     * @param sizeOfPartition int; Anzahl der Elemente pro Partition
+     * @param delegate {@link List}
+     * @param partitionSize int; Anzahl der Elemente pro Partition
      */
-    public Partition(final List<T> list, final int sizeOfPartition)
+    public Partition(final List<T> delegate, final int partitionSize)
     {
 
         super();
 
-        this.list = Objects.requireNonNull(list, "list required");
+        this.delegate = Objects.requireNonNull(delegate, "delegate required");
 
-        if (sizeOfPartition <= 0)
+        if (partitionSize < 1)
         {
-            throw new IllegalArgumentException("Size must be greater than 0");
+            throw new IllegalArgumentException("partitionSize < 1");
         }
 
-        this.sizeOfPartition = sizeOfPartition;
+        this.partitionSize = partitionSize;
+        this.partitionCount = partitionCount(delegate, this.partitionSize);
     }
 
     /**
@@ -58,19 +83,14 @@ public class Partition<T> extends AbstractList<List<T>>
             return true;
         }
 
-        if (!super.equals(obj))
-        {
-            return false;
-        }
-
-        if (!(obj instanceof Partition))
+        if (!super.equals(obj) || !(obj instanceof Partition))
         {
             return false;
         }
 
         Partition<?> other = (Partition<?>) obj;
 
-        return Objects.equals(this.list, other.list) && (this.sizeOfPartition == other.sizeOfPartition);
+        return Objects.equals(this.delegate, other.delegate) && (this.partitionSize == other.partitionSize);
     }
 
     /**
@@ -79,27 +99,10 @@ public class Partition<T> extends AbstractList<List<T>>
     @Override
     public List<T> get(final int index)
     {
-        final int listSize = size();
+        final int start = index * this.partitionSize;
+        final int end = Math.min(start + this.partitionSize, this.delegate.size());
 
-        if (listSize < 0)
-        {
-            throw new IllegalArgumentException("negative size: " + listSize);
-        }
-
-        if (index < 0)
-        {
-            throw new IndexOutOfBoundsException("Index " + index + " must not be negative");
-        }
-
-        if (index >= listSize)
-        {
-            throw new IndexOutOfBoundsException("Index " + index + " must be less than size " + listSize);
-        }
-
-        final int start = index * this.sizeOfPartition;
-        final int end = Math.min(start + this.sizeOfPartition, this.list.size());
-
-        return this.list.subList(start, end);
+        return this.delegate.subList(start, end);
     }
 
     /**
@@ -111,9 +114,7 @@ public class Partition<T> extends AbstractList<List<T>>
         final int prime = 31;
         int result = super.hashCode();
 
-        result = (prime * result) + Objects.hash(this.list, this.sizeOfPartition);
-
-        return result;
+        return (prime * result) + Objects.hash(this.delegate, this.partitionSize);
     }
 
     /**
@@ -122,7 +123,7 @@ public class Partition<T> extends AbstractList<List<T>>
     @Override
     public boolean isEmpty()
     {
-        return this.list.isEmpty();
+        return this.delegate.isEmpty();
     }
 
     /**
@@ -131,7 +132,6 @@ public class Partition<T> extends AbstractList<List<T>>
     @Override
     public int size()
     {
-        // Math.ceil(
-        return ((this.list.size() + this.sizeOfPartition) - 1) / this.sizeOfPartition;
+        return this.partitionCount;
     }
 }
