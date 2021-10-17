be4e05dab2acaa0f6ecfc6c003f3839694df64cc
tfreese <commercial@freese-home.de>
2021-10-17 16:39:28 +0200

    embedded database test

---
+++ base-persistence/src/test/java/de/freese/base/persistence/jdbc/MultiDatabaseExtension.java
@@ -0,0 +1,88 @@
+// Created: 05.04.2021
+package de.freese.base.persistence.jdbc;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
+import org.junit.jupiter.api.extension.Extension;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+/**
+ * Dient zum starten und runterfahtren der DB-Instanzen.
+ *
+ * @author Thomas Freese
+ */
+public class MultiDatabaseExtension implements BeforeAllCallback, AfterAllCallback // , ArgumentsProvider
+{
+    /**
+     *
+     */
+    private final Map<EmbeddedDatabaseType, DbServerExtension> servers = new HashMap<>();
+
+    /**
+     * Die Junit-{@link Extension} braucht zwingend einen Default-Constructor !
+     */
+    public MultiDatabaseExtension()
+    {
+        super();
+
+        this.servers.computeIfAbsent(EmbeddedDatabaseType.H2, DbServerExtension::new);
+        this.servers.computeIfAbsent(EmbeddedDatabaseType.HSQL, DbServerExtension::new);
+        this.servers.computeIfAbsent(EmbeddedDatabaseType.DERBY, DbServerExtension::new);
+    }
+
+    /**
+     * @see org.junit.jupiter.api.extension.AfterAllCallback#afterAll(org.junit.jupiter.api.extension.ExtensionContext)
+     */
+    @Override
+    public void afterAll(final ExtensionContext context) throws Exception
+    {
+        for (DbServerExtension server : this.servers.values())
+        {
+            server.afterAll(context);
+        }
+    }
+
+    /**
+     * @see org.junit.jupiter.api.extension.BeforeAllCallback#beforeAll(org.junit.jupiter.api.extension.ExtensionContext)
+     */
+    @Override
+    public void beforeAll(final ExtensionContext context) throws Exception
+    {
+        for (DbServerExtension server : this.servers.values())
+        {
+            server.beforeAll(context);
+        }
+    }
+
+    /**
+     * @param databaseType {@link EmbeddedDatabaseType}
+     *
+     * @return {@link DbServerExtension}
+     */
+    public DbServerExtension getServer(final EmbeddedDatabaseType databaseType)
+    {
+        return this.servers.get(databaseType);
+    }
+
+    /**
+     * @return {@link Collection}
+     */
+    public Collection<DbServerExtension> getServers()
+    {
+        return this.servers.values();
+    }
+
+    // /**
+    // * @see org.junit.jupiter.params.provider.ArgumentsProvider#provideArguments(org.junit.jupiter.api.extension.ExtensionContext)
+    // */
+    // @Override
+    // public Stream<? extends Arguments> provideArguments(final ExtensionContext context) throws Exception
+    // {
+    // return getServers().stream().map(server -> Arguments.of(server.getDatabaseType(), server));
+    // }
+}
