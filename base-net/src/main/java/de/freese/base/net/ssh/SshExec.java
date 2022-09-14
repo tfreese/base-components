2242b87a36d8b596107eef54167ba99e90a36d91
tfreese <commercial@freese-home.de>
2022-09-14 18:33:10 +0200

    jsch replaced with apache-sshd

---
+++ base-net/src/main/java/de/freese/base/net/ssh/SshExec.java
@@ -0,0 +1,186 @@
+package de.freese.base.net.ssh;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.Consumer;
+
+import org.apache.sshd.client.SshClient;
+import org.apache.sshd.client.channel.ChannelExec;
+import org.apache.sshd.client.channel.ClientChannelEvent;
+import org.apache.sshd.client.config.hosts.ConfigFileHostEntryResolver;
+import org.apache.sshd.client.future.ConnectFuture;
+import org.apache.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
+import org.apache.sshd.client.session.ClientSession;
+import org.apache.sshd.common.config.keys.FilePasswordProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Util-Klasse für das Aufrufen von Commandos über eine SSH-Verbindung.<br>
+ * <a href="https://github.com/apache/mina-sshd/blob/master/docs/client-setup.md">client-setup</a>
+ *
+ * @author Thomas Freese
+ */
+public final class SshExec
+{
+    /**
+     *
+     */
+    private static final Logger LOGGER = LoggerFactory.getLogger(SshExec.class);
+
+    public static SshExec connectByUserCertificate(final String user, final CharSequence password, final String host, final int port) throws IOException
+    {
+        return connect(user, host, port, sshClient ->
+        {
+            // HOST is configured in ~/.ssh/config.
+            sshClient.setHostConfigEntryResolver(new ConfigFileHostEntryResolver(Paths.get(System.getProperty("user.home"), ".ssh", "config")));
+
+            // Password for Certificate.
+            sshClient.setFilePasswordProvider(FilePasswordProvider.of(password.toString()));
+        }, clientSession ->
+        {
+            // Empty
+        });
+    }
+
+    public static SshExec connectByUserPassword(final String user, final CharSequence password, final String host, final int port) throws IOException
+    {
+        return connect(user, host, port, sshClient ->
+        {
+            // Empty
+        }, clientSession ->
+        {
+            // Only for User/Password authentication without certificate.
+            clientSession.addPasswordIdentity(password.toString());
+        });
+    }
+
+    private static SshExec connect(final String user, final String host, final int port, final Consumer<SshClient> sshClientConfigurer, Consumer<ClientSession> clientSessionConfigurer) throws IOException
+    {
+        LOGGER.debug("connecting to {}@{}", user, host);
+
+        SshClient sshClient = SshClient.setUpDefaultClient();
+        sshClient.setServerKeyVerifier(AcceptAllServerKeyVerifier.INSTANCE);
+
+        sshClientConfigurer.accept(sshClient);
+
+        // Open the client
+        sshClient.start();
+
+        // Connect to the server
+        ConnectFuture cf = sshClient.connect(user, host, port);
+        ClientSession clientSession = cf.verify().getSession();
+
+        clientSessionConfigurer.accept(clientSession);
+
+        clientSession.auth().verify(Duration.ofSeconds(5));
+
+        LOGGER.debug("session connected");
+
+        return new SshExec(sshClient, clientSession, host);
+    }
+
+    /**
+     *
+     */
+    private final ClientSession clientSession;
+    /**
+     *
+     */
+    private final String host;
+    /**
+     *
+     */
+    private final SshClient sshClient;
+
+    /**
+     *
+     */
+    private SshExec(SshClient sshClient, ClientSession clientSession, final String host)
+    {
+        super();
+
+        this.sshClient = Objects.requireNonNull(sshClient, "sshClient required");
+        this.clientSession = Objects.requireNonNull(clientSession, "clientSession required");
+        this.host = Objects.requireNonNull(host, "host required");
+    }
+
+    /**
+     * Verbindung beenden.
+     */
+    public void disconnect()
+    {
+        LOGGER.debug("disconnecting session");
+
+        try
+        {
+            if (this.clientSession.isOpen())
+            {
+                clientSession.close(false).await(Duration.ofSeconds(3));
+            }
+
+            if (this.sshClient.isOpen())
+            {
+                this.sshClient.stop();
+            }
+        }
+        catch (IOException ex)
+        {
+            LOGGER.error(ex.getMessage(), ex);
+        }
+
+        LOGGER.debug("session disconnected");
+    }
+
+    /**
+     * Ausführung des Commandos.<br>
+     *
+     * @param command String
+     *
+     * @return {@link List}
+     *
+     * @throws IOException Falls was schiefgeht.
+     */
+    public String execute(final String command) throws IOException
+    {
+        //        this.clientSession.createChannel(Channel.CHANNEL_EXEC);
+
+        try (ChannelExec channelExec = this.clientSession.createExecChannel(command);
+             ByteArrayOutputStream responseStream = new ByteArrayOutputStream();
+             ByteArrayOutputStream errorStream = new ByteArrayOutputStream())
+        {
+            channelExec.setOut(responseStream);
+            channelExec.setErr(errorStream);
+
+            // Execute and wait
+            channelExec.open();
+
+            Duration timeout = Duration.ofSeconds(3);
+            Set<ClientChannelEvent> events = channelExec.waitFor(EnumSet.of(ClientChannelEvent.CLOSED), timeout);
+
+            // Check if timed out
+            if (events.contains(ClientChannelEvent.TIMEOUT))
+            {
+                throw new IOException(String.format("Timeout after %d seconds on host '%s' for command '%s'", timeout.toSeconds(), this.host, command));
+            }
+
+            errorStream.flush();
+            String error = errorStream.toString(StandardCharsets.UTF_8);
+
+            if (!error.isEmpty())
+            {
+                throw new IOException(error);
+            }
+
+            responseStream.flush();
+            return responseStream.toString(StandardCharsets.UTF_8);
+        }
+    }
+}
---
+++ base-net/src/test/java/de/freese/base/net/ssh/TestSshExec.java
@@ -0,0 +1,63 @@
+package de.freese.base.net.ssh;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+/**
+ * @author Thomas Freese
+ */
+@TestMethodOrder(MethodOrderer.MethodName.class)
+@Disabled("No passwords")
+class TestSshExec
+{
+    /**
+     * @throws Exception Falls was schiefgeht.
+     */
+    @Test
+    @EnabledOnOs(
+            {
+                    OS.LINUX, OS.MAC
+            })
+    void testSshUserCertificate() throws Exception
+    {
+        SshExec sshExec = SshExec.connectByUserCertificate("user", "pass", "remote", 22);
+
+        String result = sshExec.execute("df -h");
+
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+
+        System.out.println(result);
+
+        sshExec.disconnect();
+    }
+
+    /**
+     * @throws Exception Falls was schiefgeht.
+     */
+    @Test
+    @EnabledOnOs(
+            {
+                    OS.LINUX, OS.MAC
+            })
+    void testSshUserPassword() throws Exception
+    {
+        SshExec sshExec = SshExec.connectByUserPassword("user", "pass", "remote", 22);
+
+        String result = sshExec.execute("df -h");
+
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+
+        System.out.println(result);
+
+        sshExec.disconnect();
+    }
+}
