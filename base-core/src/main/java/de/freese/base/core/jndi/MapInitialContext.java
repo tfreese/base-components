78e6edb3667a4c2626e483ba5057ebe781331cd1
tfreese <community@freese-home.de>
2022-12-04 17:33:45 +0100

    MapInitialContext

---
+++ base-core/src/main/java/de/freese/base/core/jndi/MapInitialContext.java
@@ -0,0 +1,73 @@
+package de.freese.base.core.jndi;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.naming.spi.InitialContextFactory;
+import javax.naming.spi.InitialContextFactoryBuilder;
+import javax.naming.spi.NamingManager;
+
+/**
+ * <pre>{@code
+ * MapInitialContext.init();
+ *
+ * Context initialContext = new InitialContext();
+ * initialContext.bind("java:comp/env/...", ...);
+ * }</pre>
+ *
+ * @author Thomas Freese
+ */
+public final class MapInitialContext extends InitialContext
+{
+    public static void init() throws NamingException
+    {
+        final InitialContext initialContext = new MapInitialContext();
+
+        InitialContextFactory factory = environment -> initialContext;
+        InitialContextFactoryBuilder builder = environment -> factory;
+        NamingManager.setInitialContextFactoryBuilder(builder);
+
+        //        NamingManager.setInitialContextFactoryBuilder(environment -> environment1 -> initialContext);
+    }
+
+    private final Map<String, Object> cache = new ConcurrentHashMap<>();
+
+    private MapInitialContext() throws NamingException
+    {
+        super();
+    }
+
+    @Override
+    public void bind(String key, Object value)
+    {
+        cache.put(key, value);
+    }
+
+    @Override
+    public Object lookup(String key) throws NamingException
+    {
+        return cache.get(key);
+    }
+
+    @Override
+    public void rebind(final String name, final Object obj) throws NamingException
+    {
+        bind(name, obj);
+    }
+
+    @Override
+    public void rename(final String oldName, final String newName) throws NamingException
+    {
+        Object value = lookup(oldName);
+        unbind(oldName);
+        bind(newName, value);
+    }
+
+    @Override
+    public void unbind(final String name) throws NamingException
+    {
+        cache.remove(name);
+    }
+}
