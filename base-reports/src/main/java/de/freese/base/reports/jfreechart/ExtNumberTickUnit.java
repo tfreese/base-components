f7cfee9ad4b6ee61b8f119db5d8ecfeb85034007
tfreese <community@freese-home.de>
2024-02-18 09:43:28 +0100

    polish

--- base-reports/src/main/java/de/freese/base/reports/jfreechart/ExtNumberTickUnit.java
+++ base-reports/src/main/java/de/freese/base/reports/jfreechart/ExtNumberTickUnit.java
@@ -9,7 +9,7 @@ import java.util.Set;
 import org.jfree.chart.axis.NumberTickUnit;
 
 /**
- * {@link NumberTickUnit} to disable Text for defined Ticks ({@link #setExcludes(Set<String>)}).
+ * {@link NumberTickUnit} to disable Text for defined Ticks ({@link #setExcludes(Set)}).
  *
  * @author Thomas Freese
  */
@@ -17,7 +17,7 @@ public class ExtNumberTickUnit extends NumberTickUnit {
     @Serial
     private static final long serialVersionUID = 8151941607328082952L;
 
-    private Set<String> excludes = Collections.emptySet();
+    private transient Set<String> excludes = Collections.emptySet();
 
     public ExtNumberTickUnit(final double size) {
         super(size);
@@ -29,6 +29,28 @@ public class ExtNumberTickUnit extends NumberTickUnit {
 
     public ExtNumberTickUnit(final double size, final NumberFormat formatter, final int minorTickCount) {
         super(size, formatter, minorTickCount);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof final ExtNumberTickUnit that)) {
+            return false;
+        }
+
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        return Objects.equals(excludes, that.excludes);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), excludes);
     }
 
     public void setExcludes(final Set<String> excludes) {
